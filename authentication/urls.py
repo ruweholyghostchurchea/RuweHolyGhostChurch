133bc57258faeffa35d1d18d8944d7f9a314e20c
Ruwe Holy Ghost Church EA <ruweholyghostchurchea@gmail.com>
2025-10-21 22:04:36 +0300

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('reset/<uidb64>/<token>/', views.password_reset_confirm_view, name='password_reset_confirm'),
 ]
